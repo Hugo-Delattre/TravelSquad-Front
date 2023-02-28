812215999d6424008e18c24be6c21aea971d2da5
unknown <badrehahaha@gmail.com>
2023-02-28 10:44:25 +0100

    save

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./styles/index.scss";
 import "semantic-ui-css/semantic.min.css";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
